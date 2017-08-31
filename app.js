2c659492354392dfccdb310facbdb51efbe025e0
Connor Knabe <connorknabe@gmail.com>
2017-08-31 12:39:22 -0500

    adding twilio

--- app.js
+++ app.js
@@ -7,22 +7,51 @@ var path = require('path');
 var Gpio = require('onoff').Gpio;
 var request = require('request');
 var login = require('./login.js');
-var bodyParser = require('body-parser')
-var session = require('express-session')
-var cookieParser = require('cookie-parser')
+var twilioLoginInfo = require('./settings/twilioLoginInfo.js');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var spawn = require('child_process').spawn;
+var proc;
 
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use(session({
   secret: login.secret,
   resave: false,
   saveUninitialized: true,
   cookie: { secure: false }
-}))
-// var garageSensor = new Gpio(14, 'in');
+}));
 
-var spawn = require('child_process').spawn;
-var proc;
+var garageSensor = new Gpio(21, 'in','both');
+var garageSwitch = new Gpio(24, 'in','both');
+
+
+var hasSent = false;
+
+garageSensor.watch(function(err, value) {
+   if (value==1 && !hasSent){
+       var curTime = new Date();
+       hasSent = true;
+       console.log("Garge door opened "+value+new Date());
+       sendMessage(twilioLoginInfo.toNumbers,"");
+   }
+});
+
+
+function sendMessage(numbers, msgContent){
+    if(numbers) {
+		for (var i = 0; i < numbers.length; i++) {
+            if(numbers[i].email){
+                sendEmail(numbers[i],msgContent);
+            }
+            if(numbers[i].number){
+                sendText(numbers[i],msgContent);
+            }
+		}
+	}
+}
+
 
 app.use('/', express.static(path.join(__dirname, 'stream')));
 // app.use(bodyParser.urlencoded())
@@ -33,16 +62,12 @@ app.use(bodyParser.urlencoded({
 
 app.get('/', function(req, res) {
 	if((req.session && req.session.userInfo && req.session.userInfo.username === login.username && req.session.userInfo.password === login.password) || req.cookies.holkaCookie === login.secretCookie){
-		
-
 	    var options = {
-	        maxAge: 1000 * 60* 60 * 24  * 180, 
+	        maxAge: 1000 * 60* 60 * 24  * 180,
 	        httpOnly: true
-	    }
-			
-		res.cookie('holkaCookie', login.secretCookie, options)
+	    };
+		res.cookie('holkaCookie', login.secretCookie, options);
 
-		
 		console.log('cookies',req.cookies.holkaCookie);
 // 		cookies.forEach(console.log);
 		res.sendFile(__dirname + '/admin.html');
@@ -74,7 +99,7 @@ io.on('connection', function(socket) {
     delete sockets[socket.id];
 
     // no more sockets, kill the stream
-    if (Object.keys(sockets).length == 0) {
+    if (Object.keys(sockets).length === 0) {
       app.set('watchingFile', false);
       if (proc) proc.kill();
       fs.unwatchFile('./stream/image_stream.jpg');
@@ -92,7 +117,7 @@ http.listen(3000, function() {
 });
 
 function stopStreaming() {
-  if (Object.keys(sockets).length == 0) {
+  if (Object.keys(sockets).length === 0) {
     app.set('watchingFile', false);
     if (proc) proc.kill();
     fs.unwatchFile('./stream/image_stream.jpg');
@@ -101,29 +126,34 @@ function stopStreaming() {
 
 function startStreaming(io) {
 
-  if (app.get('watchingFile')) {
+    if (app.get('watchingFile')) {
+        io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
+        return;
+    }
+
+    var timeStamp = new Date();
+
+    var timeArr = [];
+
+    timeArr.push(timeStamp);
+
+    var args = ["-w", "2592", "-h", "1944", "-vf", "-hf", "-o", "./stream/image_stream.jpg", "-t", "999999999", "-tl", "3000", "-ex","night"];
+    proc = spawn('raspistill', args);
+
+    console.log('Watching for changes...');
+
+    app.set('watchingFile', true);
+
+    fs.watchFile('./stream/image_stream.jpg', function(current, previous) {
     io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
-    return;
-  }
 
-  var args = ["-w", "640", "-h", "480", "-vf", "-hf", "-o", "./stream/image_stream.jpg", "-t", "999999999", "-tl", "100"];
-  proc = spawn('raspistill', args);
-
-  console.log('Watching for changes...');
-
-  app.set('watchingFile', true);
-
-  fs.watchFile('./stream/image_stream.jpg', function(current, previous) {
-    io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
-    
     fs.stat("./stream/image_stream.jpg", function(err, stats){
-	    var mtime = new Date(stats.mtime);
-	    console.log(mtime.toString());
-		io.sockets.emit('liveStreamDate', mtime.toString());
+        var mtime = new Date(stats.mtime);
+        console.log(mtime.toString());
+    	io.sockets.emit('liveStreamDate', mtime.toString());
+    });
 
-	});
-
-  })
+});
 
 }
 
@@ -140,6 +170,3 @@ garageSensor.watch(function(err, val){
 	console.log('val',val);
 });
 */
-
-
-
