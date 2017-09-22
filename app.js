ca9a67356ac6a968b04592aacb7499d483612c3a
Connor Knabe <connorknabe@gmail.com>
2017-09-22 08:37:46 -0500

    About to implement auth0
    Conflicts:
    	admin.html
    	app.js
    	package.json

--- app.js
+++ app.js
@@ -42,14 +42,14 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var spawn = require('child_process').spawn;
+
 var proc;
 var port = 3000;
-var debugMode = true;
 var log4js = require('log4js');
 var logger = log4js.getLogger();
 logger.level = 'debug';
-
-
+var debugMode = true;
+var hoursToWaitBeforeNextSecurityAlert = 2;
 
 app.use(cookieParser());
 >>>>>>> ee556a5... adding twilio
@@ -66,6 +66,7 @@ app.use(session({
 var garageSensor = new Gpio(21, 'in','both');
 var garageSwitch = new Gpio(24, 'in','both');
 <<<<<<< HEAD
+<<<<<<< HEAD
 var garageTimeout;
 var securityMsgTimeout = null;
 var shouldSendSecurityAlert = true;
@@ -78,6 +79,11 @@ var shouldSendSecurityAlert = true;
 
 
 >>>>>>> 54b6e6d... Adding logic to open garage via post route
+=======
+var garageTimeout;
+var securityMsgTimeout = null;
+var shouldSendSecurityAlert = true;
+>>>>>>> c9b90e5... About to implement auth0
 var hasSent = false;
 
 garageSensor.watch(function(err, value) {
@@ -110,17 +116,17 @@ function sendMessage(numbers, msgContent){
    }
 });
 
-sendMessage(twilioLoginInfo.toNumbers,'Garage closed');
-
-
-
+// sendMessage(twilioLoginInfo.toNumbers,'Garage closed');
 function sendMessage(numbers, msgContent){
+<<<<<<< HEAD
 <<<<<<< HEAD
     console.log('numbers',numbers);
 >>>>>>> 6edde25... Changing security for image
 =======
     logger.info('Sending text message containing', msgContent);
 >>>>>>> 54b6e6d... Adding logic to open garage via post route
+=======
+>>>>>>> c9b90e5... About to implement auth0
     if(numbers) {
 		for (var i = 0; i < numbers.length; i++) {
             if(numbers[i].email){
@@ -208,6 +214,7 @@ function sendEmail(alertInfo, msgContent){
 }
 
 app.use('/', express.static(path.join(__dirname, 'js')));
+<<<<<<< HEAD
 // app.use(bodyParser.urlencoded())
 app.use(bodyParser.urlencoded({
 	extended: true
@@ -359,16 +366,20 @@ function sendMessage(numbers, msgContent){
 
 >>>>>>> 6edde25... Changing security for image
 app.use('/', express.static(path.join(__dirname, 'stream')));
+=======
+>>>>>>> c9b90e5... About to implement auth0
 // app.use(bodyParser.urlencoded())
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
 function auth(req){
-    return (req.session && req.session.userInfo && req.session.userInfo.username === login.username) || req.cookies.holkaCookie === login.secretCookie;
+    var authenticated = (req.session && req.session.userInfo && req.session.userInfo.username === login.username) && req.cookies.holkaCookie === login.secretCookie;
+    logger.debug('isauth', authenticated);
+    return authenticated;
 }
 app.get('/', function(req, res) {
-	if(auth){
+	if(auth(req)){
 	    var options = {
 	        maxAge: 1000 * 60* 60 * 24  * 180,
 	        httpOnly: true
@@ -397,34 +408,48 @@ function garageIsOpen(){
 }
 
 app.post('/openOrCloseGarage', function(req,res){
-    var garageStatus = 'opening';
+    var garageStatus = null;
+    logger.debug('body',req.body);
     if(auth(req)){
-        if(req.body && req.body.shouldOpen && !garageIsOpen()){
-            openOrCloseGarage();
-        } else if(req.body && req.body.shouldClose && garageIsOpen()){
-            openOrCloseGarage();
+        if(req.body && req.body.garageSwitch == 'open' && !garageIsOpen()){
+            toggleGarageDoor();
+             garageStatus = 'opening';
+        } else if(req.body && req.body.garageSwitch == 'close' && garageIsOpen()){
+            toggleGarageDoor();
             garageStatus = 'closing';
         }
         var msg = garageStatus+' garage via button';
-        sendMessage(numbers,msg);
+        if(garageStatus){
+            sendMessage(twilioLoginInfo.toNumbers,msg);
+        }
         logger.info(msg);
+        res.send('auth');
     } else {
+        var securityMsg = 'SECURITY: tried to open garage via post without being authenticated!!';
+        clearTimeout(securityMsgTimeout);
+
+        securityMsgTimeout = setTimeout(function(){
+            shouldSendSecurityAlert = true;
+        },hoursToWaitBeforeNextSecurityAlert*60*60*10000);
+
+        if(shouldSendSecurityAlert){
+            sendMessage(twilioLoginInfo.toNumbers,securityMsg);
+            shouldSendSecurityAlert = false;
+        }
+        logger.fatal(securityMsg,'Ip address is: ',req.headers['x-forwaded-for'],'or: ',req.connection.remoteAddress);
         res.status(401);
         res.send('not auth');
-        var securityMsg = 'SECURITY: tried to open garage via post without being authenticated!!';
-        sendMessage(numbers,securityMsg);
-        logger.fatal(securityMsg,'Ip address is: ',req.headers['x-forwaded-for'],'or: ',req.connection.remoteAddress);
     }
 });
 
-var garageTimeout;
 function toggleGarageDoor(){
     clearTimeout(garageTimeout);
-    garageSwitch.writeSync(1);
-
-    garageTimeout = setTimeout(function(){
-        garageSwitch.writeSync(0);
-    },1000);
+    if(!debugMode){
+        garageSwitch.writeSync(1);
+        garageTimeout = setTimeout(function(){
+            garageSwitch.writeSync(0);
+        },1000);
+    }
 }
 
 var sockets = {};
@@ -495,12 +520,15 @@ app.get('/stream/image_stream.jpg',function(req,res){
 app.get('/stream/image_stream.jpg',function(req,res){
     if(auth(req)){
 <<<<<<< HEAD
+<<<<<<< HEAD
 
         console.log("req",req.headers['x-forwaded-for'],req.connection.remoteAddress);
 >>>>>>> 6edde25... Changing security for image
 =======
         logger.debug('req',req.headers['x-forwaded-for'],req.connection.remoteAddress);
 >>>>>>> 54b6e6d... Adding logic to open garage via post route
+=======
+>>>>>>> c9b90e5... About to implement auth0
         fs.readFile('./stream/image_stream.jpg', function(err, data) {
           if (err) throw err; // Fail if the file can't be read.
             res.writeHead(200, {'Content-Type': 'image/jpeg'});
@@ -515,7 +543,11 @@ app.get('/stream/image_stream.jpg',function(req,res){
 =======
 >>>>>>> 54b6e6d... Adding logic to open garage via post route
     } else{
+<<<<<<< HEAD
 >>>>>>> 6edde25... Changing security for image
+=======
+        logger.fatal('Unauthorized request for image_stream.jpg',req.headers['x-forwaded-for'],req.connection.remoteAddress);
+>>>>>>> c9b90e5... About to implement auth0
         res.status(401);
         res.send('not auth');
     }
