b81bedee7019f83c73f2596fc8733d5f37727f20
Connor Knabe <connorknabe@gmail.com>
2017-10-01 14:52:50 -0500

    About to add new streaming method

--- services/iot.js
+++ services/iot.js
@@ -17,6 +17,10 @@ var garageSensorTimeoutOne = null;
 var garageSensorTimeoutTwo = null;
 var raspistillProc;
 
+
+//uv4l -nopreview --auto-video_nr --driver raspicam --encoding mjpeg --width 640 --height 480 --framerate 20 --server-option '--port=9090' --server-option '--max-queued-connections=30' --server-option '--max-streams=25' --server-option '--max-threads=29' --vflip yes --hflip yes
+
+
 module.exports = function(app,enableMotionSensor,debugMode,io,logger) {
     var hasBeenOpened = garageIsOpen();
 	var messenger = require('./messenger.js')(logger,debugMode);
