7bcc20cb1937429930c4975004bf4711fe22e263
Connor Knabe <connorknabe@gmail.com>
2017-10-04 06:50:56 -0500

    inlining live viewer text

--- cam.sh
+++ cam.sh
@@ -2,7 +2,7 @@
 
 pkill uv4l
 
-uv4l -nopreview --auto-video_nr --driver raspicam --encoding mjpeg --width 640 --height 480 --framerate 20 --server-option '--port=9090' --server-option '--max-queued-connections=30' --server-option '--max-streams=25' --server-option '--max-threads=29' --vflip yes --hflip yes
+uv4l -nopreview --auto-video_nr --driver raspicam --encoding mjpeg --width 640 --height 480 --framerate 30 --server-option '--port=9090' --server-option '--max-queued-connections=30' --server-option '--max-streams=25' --server-option '--max-threads=29' --vflip yes --hflip yes
 
 
 while true
