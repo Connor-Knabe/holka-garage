3fa5d9968ab4d3e29b3225eed09c1a9545d3382c
Connor Knabe <connorknabe@gmail.com>
2017-10-26 07:34:31 -0500

    Adding options for enabling hue

--- services/hue.js
+++ services/hue.js
@@ -1,64 +1,67 @@
 var login = require('../settings/login.js'),
-	hue = require("node-hue-api");
+    hue = require('node-hue-api'),
+    options = require('../settings/options.js');
+
 var HueApi = hue.HueApi,
     lightState = hue.lightState,
-	host = login.hueBridgeIp,
+    host = login.hueBridgeIp,
     username = login.hueUser,
     api = new HueApi(host, username),
-	state = lightState.create(),
-	lightsOffTimeout = null;
-	
+    state = lightState.create(),
+    lightsOffTimeout = null;
+
 module.exports = function(logger) {
-	function garageLightsOn15(){
-		lightsOn();
-		logger.debug('Lights on for '+login.garageLightTimeoutMins+' mins');
+    function garageLightsOn15() {
+        if (options.enableHue) {
+            lightsOn();
+            logger.debug(
+                'Lights on for ' + login.garageLightTimeoutMins + ' mins'
+            );
+            clearTimeout(lightsOffTimeout);
+            lightsOffTimeout = setTimeout(function() {
+                lightsOff();
+            }, login.garageLightTimeoutMins * 60 * 1000);
+        }
+    }
 
-		clearTimeout(lightsOffTimeout);
-		lightsOffTimeout = setTimeout(function(){
-			lightsOff();
-		}, login.garageLightTimeoutMins*60*1000);			
-	}
+    function garageLightsOnOff(toggleOn) {
+        if (options.enableHue) {
+            if (toggleOn) {
+                lightsOn();
+                logger.debug('lights on');
+            } else {
+                lightsOff();
+                logger.debug('lights off');
+            }
+        }
+    }
 
-	
-	function garageLightsOnOff(toggleOn){
-		
-		if(toggleOn){
-			lightsOn();
-			logger.debug('lights on');
-		} else {
-			lightsOff();
-			logger.debug('lights off');
-		}
-			
-	}
+    var displayResult = function(result) {
+        logger.debug(result);
+    };
 
-	var displayResult = function(result) {
-    	logger.debug(result);
-	};
-	
-	var displayError = function(err) {
-	    logger.error(err);
-	};
-	
-	
-	
-	function lightsOn(){
-		api.setGroupLightState(8, state.on())
-			.then()
-		    .fail(displayError)
-		    .done();
-	}
+    var displayError = function(err) {
+        logger.error(err);
+    };
 
-	function lightsOff(){
-		api.setGroupLightState(8, state.off())
-			.then()
-		    .fail(displayError)
-		    .done();
-	}
-	
-	return {
-		garageLightsOnOff: garageLightsOnOff,
-		garageLightsOn15:garageLightsOn15
-	}
+    function lightsOn() {
+        api
+            .setGroupLightState(8, state.on())
+            .then()
+            .fail(displayError)
+            .done();
+    }
 
-}
+    function lightsOff() {
+        api
+            .setGroupLightState(8, state.off())
+            .then()
+            .fail(displayError)
+            .done();
+    }
+
+    return {
+        garageLightsOnOff: garageLightsOnOff,
+        garageLightsOn15: garageLightsOn15
+    };
+};
