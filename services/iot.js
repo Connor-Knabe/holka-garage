de41dad43dec89443df21d4caf98c1afad70112e
Connor Knabe <ConnorKnabe@gmail.com>
2019-08-07 08:14:36 -0500

    update

--- services/iot.js
+++ services/iot.js
@@ -6,226 +6,232 @@ const garageSensor = new Gpio(4, 'in', 'both');
 const garageSwitch = new Gpio(21, 'out');
 
 var garageTimeout = null,
-    motionSensorTimeoutOne = null,
-    motionSensorTimeoutTwo = null,
-    hasTurnedLightsOn = false,
-    shouldSendGarageDoorAlertOne = true,
-    shouldSendGarageDoorAlertTwo = true,
-    garageSensorTimeoutOne = null,
-    garageSensorTimeoutTwo = null,
-    garageOpened = false,
-    garageOpenAlertTimeout = null,
-    garageOpenAlertManualEnable = false,
-    garageOpenAlertPersonTwoManualEnable = false,
-    manualButtonToggle = false,
-    manualGarageOpenTimeout = null,
-    homeIsOccupied = true,
-    isHomeAwayEnabled = true,
-    homeEnabledTimeout = null;
+	motionSensorTimeoutOne = null,
+	motionSensorTimeoutTwo = null,
+	hasTurnedLightsOn = false,
+	shouldSendGarageDoorAlertOne = true,
+	shouldSendGarageDoorAlertTwo = true,
+	garageSensorTimeoutOne = null,
+	garageSensorTimeoutTwo = null,
+	garageOpened = false,
+	garageOpenAlertTimeout = null,
+	garageOpenAlertManualEnable = false,
+	garageOpenAlertPersonTwoManualEnable = false,
+	manualButtonToggle = false,
+	manualGarageOpenTimeout = null,
+	homeIsOccupied = true,
+	isHomeAwayEnabled = true,
+	homeEnabledTimeout = null;
 
 module.exports = function(app, debugMode, io, logger) {
-    var hasBeenOpened = garageIsOpen();
-    const messenger = require('./messenger.js')(logger, debugMode);
-    const messengerInfo = require('../settings/messengerInfo.js');
-    const options = require('../settings/options.js');
-    const hue = require('./hue.js')(logger);
-    const video = require('./video.js')(app, logger, io);
-    const rp = require('request-promise');
-    app.set('takingVideo', false);
+	var hasBeenOpened = garageIsOpen();
+	const messenger = require('./messenger.js')(logger, debugMode);
+	const messengerInfo = require('../settings/messengerInfo.js');
+	const options = require('../settings/options.js');
+	const hue = require('./hue.js')(logger);
+	const video = require('./video.js')(app, logger, io);
+	const rp = require('request-promise');
+	app.set('takingVideo', false);
 
-    garageSensor.watch(function(err, value) {
-        if (err) {
-            logger.error('Error watching garage sensor: ', err);
-        }
-        if (value == 1 && !hasBeenOpened) {
-            hasBeenOpened = true;
-            garageOpened = true;
-            var msg = 'Garage door opened';
-            if (options.enableLightsOnGarageOpen) {
-                hue.garageLightsOnTimed(25);
-            }
-            clearTimeout(garageSensorTimeoutOne);
-            garageSensorTimeoutOne = setTimeout(() => {
-                shouldSendGarageDoorAlertOne = true;
-            }, 1 * 60 * 10000);
-            logger.debug('garage open');
-            clearTimeout(garageOpenAlertTimeout);
-            garageOpenAlertTimeout = setTimeout(() => {
-                if (garageIsOpen()) {
-                    setTimeout(() => {
-                        var garageAlertMsg = `Garage has been open for more than: ${options.garageOpenAlertMins} minutes!`;
-                        logger.debug(garageAlertMsg);
-                        if (options.garageOpenMinsAlert) {
-                            logger.debug(garageAlertMsg);
-                            video
-                                .streamVideo()
-                                .then(() => {
-                                    messenger.send(options.alertButtonPressTexts, messengerInfo.toNumbers, garageAlertMsg, options.alertSendPictureText, true);
-                                    video.stopStreaming();
-                                })
-                                .catch(() => {
-                                    garageAlertMsg = `Garage has been open for more than: ${options.garageOpenAlertMins} minutes! Error taking new video.`;
-                                    messenger.send(options.alertButtonPressTexts, messengerInfo.toNumbers, garageAlertMsg, options.alertSendPictureText, true);
-                                    video.stopStreaming();
-                                });
-                        }
-                        messenger.sendIfttGarageOpenedAlert(options.iftttSendGarageOpenAlert, options.garageOpenAlertMins);
-                    }, 30 * 1000);
-                }
-            }, options.garageOpenAlertMins * 60 * 1000);
+	garageSensor.watch(function(err, value) {
+		if (err) {
+			logger.error('Error watching garage sensor: ', err);
+		}
+		if (value == 1 && !hasBeenOpened) {
+			hasBeenOpened = true;
+			garageOpened = true;
+			var msg = 'Garage door opened';
+			if (options.enableLightsOnGarageOpen) {
+				hue.garageLightsOnTimed(25);
+			}
+			clearTimeout(garageSensorTimeoutOne);
+			garageSensorTimeoutOne = setTimeout(() => {
+				shouldSendGarageDoorAlertOne = true;
+			}, 1 * 60 * 10000);
+			logger.debug('garage open');
+			clearTimeout(garageOpenAlertTimeout);
+			garageOpenAlertTimeout = setTimeout(() => {
+				if (garageIsOpen()) {
+					setTimeout(() => {
+						var garageAlertMsg = `Garage has been open for more than: ${options.garageOpenAlertMins} minutes!`;
+						logger.debug(garageAlertMsg);
+						if (options.garageOpenMinsAlert) {
+							logger.debug(garageAlertMsg);
+							video
+								.streamVideo()
+								.then(() => {
+									messenger.send(options.alertButtonPressTexts, messengerInfo.toNumbers, garageAlertMsg, options.alertSendPictureText, true);
+									video.stopStreaming();
+								})
+								.catch(() => {
+									garageAlertMsg = `Garage has been open for more than: ${options.garageOpenAlertMins} minutes! Error taking new video.`;
+									messenger.send(options.alertButtonPressTexts, messengerInfo.toNumbers, garageAlertMsg, options.alertSendPictureText, true);
+									video.stopStreaming();
+								});
+						}
+						messenger.sendIfttGarageOpenedAlert(options.iftttSendGarageOpenAlert, options.garageOpenAlertMins);
+					}, 30 * 1000);
+				}
+			}, options.garageOpenAlertMins * 60 * 1000);
 
-            if (!manualButtonToggle) {
-                logger.debug('garage not opened via button');
-                if (shouldSendGarageDoorAlertOne && garageOpenAlertManualEnable) {
-                    logger.debug('sending garage door gps alert');
+			if (!manualButtonToggle) {
+				logger.debug('garage not opened via button');
+				if (shouldSendGarageDoorAlertOne && garageOpenAlertManualEnable) {
+					logger.debug('sending garage door gps alert');
 
-                    if (garageOpenAlertPersonTwoManualEnable) {
-                        logger.debug('about to send video message');
-                        video
-                            .streamVideo()
-                            .then(() => {
-                                var garageAlertMsg = `The garage has been opened but the homeowners are not home!`;
-                                messenger.send(true, messengerInfo.toNumbers, garageAlertMsg, options.alertSendPictureText, true);
-                                video.stopStreaming();
-                            })
-                            .catch(() => {
-                                var garageAlertMsg = `The garage has been opened but the homeowners are not home! Error taking new video.`;
-                                messenger.send(true, messengerInfo.toNumbers, garageAlertMsg, options.alertSendPictureText, true);
-                                video.stopStreaming();
-                            });
-                    } else {
-                        messenger.sendIftt(garageOpened);
-                    }
-                    shouldSendGarageDoorAlertOne = false;
-                }
-            }
+					if (garageOpenAlertPersonTwoManualEnable) {
+						logger.debug('about to send video message');
+						video
+							.streamVideo()
+							.then(() => {
+								var garageAlertMsg = `The garage has been opened but the homeowners are not home!`;
+								messenger.send(true, messengerInfo.toNumbers, garageAlertMsg, options.alertSendPictureText, true);
+								video.stopStreaming();
+							})
+							.catch(() => {
+								var garageAlertMsg = `The garage has been opened but the homeowners are not home! Error taking new video.`;
+								messenger.send(true, messengerInfo.toNumbers, garageAlertMsg, options.alertSendPictureText, true);
+								video.stopStreaming();
+							});
+					} else {
+						messenger.sendIftt(garageOpened);
+					}
+					shouldSendGarageDoorAlertOne = false;
+				}
+			}
 
-            logger.debug(msg);
-            io.sockets.emit('garageErrorStatus', null);
-        } else if (value == 0 && hasBeenOpened) {
-            hasBeenOpened = false;
-            garageOpened = false;
-            var msg = 'Garage door closed';
-            clearTimeout(garageSensorTimeoutTwo);
-            garageSensorTimeoutTwo = setTimeout(() => {
-                shouldSendGarageDoorAlertTwo = true;
-            }, 1 * 60 * 10000);
+			logger.debug(msg);
+			io.sockets.emit('garageErrorStatus', null);
+		} else if (value == 0 && hasBeenOpened) {
+			hasBeenOpened = false;
+			garageOpened = false;
+			var msg = 'Garage door closed';
+			clearTimeout(garageSensorTimeoutTwo);
+			garageSensorTimeoutTwo = setTimeout(() => {
+				shouldSendGarageDoorAlertTwo = true;
+			}, 1 * 60 * 10000);
 
-            if (shouldSendGarageDoorAlertTwo && garageOpenAlertManualEnable) {
-                if (options.alertButtonPressTexts) {
-                    messenger.send(true, messengerInfo.toNumbers, msg, false, false);
-                }
-                messenger.sendIftt(garageOpened);
-                shouldSendGarageDoorAlertTwo = false;
-            }
-            logger.debug(msg);
-            io.sockets.emit('garageErrorStatus', null);
-        }
-    });
+			if (shouldSendGarageDoorAlertTwo && garageOpenAlertManualEnable) {
+				if (options.alertButtonPressTexts) {
+					messenger.send(true, messengerInfo.toNumbers, msg, false, false);
+				}
+				messenger.sendIftt(garageOpened);
+				shouldSendGarageDoorAlertTwo = false;
+			}
+			logger.debug(msg);
+			io.sockets.emit('garageErrorStatus', null);
+		}
+	});
 
-    if (options.enableMotionSensor && options.enableHue) {
-        motionSensor.watch((err, value) => {
-            if (err) {
-                logger.error('Error watching motion sensor: ', err);
-            }
-            //logger.debug('value:', value);
-            if (value == 1 && !hasTurnedLightsOn) {
-                clearTimeout(motionSensorTimeoutOne);
-                motionSensorTimeoutOne = setTimeout(function() {
-                    hasTurnedLightsOn = true;
-                }, 5 * 1000);
-                hue.garageLightsOnTimed(100);
-            } else if (value == 0 && hasTurnedLightsOn) {
-                clearTimeout(motionSensorTimeoutTwo);
-                motionSensorTimeoutTwo = setTimeout(function() {
-                    hasTurnedLightsOn = false;
-                }, 5 * 1000);
-            }
-        });
-    }
+	if (options.enableMotionSensor && options.enableHue) {
+		motionSensor.watch((err, value) => {
+			if (err) {
+				logger.error('Error watching motion sensor: ', err);
+			}
+			//logger.debug('value:', value);
+			if (value == 1 && !hasTurnedLightsOn) {
+				clearTimeout(motionSensorTimeoutOne);
+				motionSensorTimeoutOne = setTimeout(function() {
+					hasTurnedLightsOn = true;
+				}, 5 * 1000);
+				hue.garageLightsOnTimed(100);
+			} else if (value == 0 && hasTurnedLightsOn) {
+				clearTimeout(motionSensorTimeoutTwo);
+				motionSensorTimeoutTwo = setTimeout(function() {
+					hasTurnedLightsOn = false;
+				}, 5 * 1000);
+			}
+		});
+	}
 
-    function garageIsOpen() {
-        var isOpen = garageSensor.readSync() == 1 ? true : false;
-        return isOpen;
-    }
+	function garageIsOpen() {
+		var isOpen = garageSensor.readSync() == 1 ? true : false;
+		return isOpen;
+	}
 
-    function toggleGarageDoor() {
-        if (!debugMode) {
-            manualButtonToggle = true;
-            clearTimeout(manualGarageOpenTimeout);
-            manualGarageOpenTimeout = setTimeout(() => {
-                manualButtonToggle = false;
-            }, 60 * 1000);
-            logger.debug('Opening/closing door now');
-            garageSwitch.writeSync(1);
-            garageTimeout = setTimeout(function() {
-                garageSwitch.writeSync(0);
-            }, 1000);
-        }
-    }
+	function toggleGarageDoor() {
+		if (!debugMode) {
+			manualButtonToggle = true;
+			clearTimeout(manualGarageOpenTimeout);
+			manualGarageOpenTimeout = setTimeout(() => {
+				manualButtonToggle = false;
+			}, 60 * 1000);
+			logger.debug('Opening/closing door now');
+			garageSwitch.writeSync(1);
+			garageTimeout = setTimeout(function() {
+				garageSwitch.writeSync(0);
+			}, 1000);
+		}
+	}
 
-    function toggleGarageOpenAlert(enable) {
-        logger.debug('Enable toggleGarageOpenAlert' + enable);
-        garageOpenAlertManualEnable = enable;
-        setHome();
-    }
+	function toggleGarageOpenAlert(enable) {
+		logger.debug('Enable toggleGarageOpenAlert' + enable);
+		garageOpenAlertManualEnable = enable;
+		setHome();
+	}
 
-    function toggleGarageOpenAlertSecondPerson(enable) {
-        logger.debug('Enable toggleGarageOpenAlertSecondPerson' + enable);
-        garageOpenAlertPersonTwoManualEnable = enable;
-        setHome();
-    }
+	function toggleGarageOpenAlertSecondPerson(enable) {
+		logger.debug('Enable toggleGarageOpenAlertSecondPerson' + enable);
+		garageOpenAlertPersonTwoManualEnable = enable;
+		setHome();
+	}
 
-    function setHome() {
-        if (isHomeAwayEnabled) {
-            if (garageOpenAlertManualEnable && garageOpenAlertPersonTwoManualEnable) {
-                homeIsOccupied = false;
-            } else {
-                homeIsOccupied = true;
-            }
-            updateIftttWithHomeStatus(isHomeAwayEnabled);
-        }
-    }
+	function setHome() {
+		if (isHomeAwayEnabled) {
+			if (garageOpenAlertManualEnable && garageOpenAlertPersonTwoManualEnable) {
+				if (homeIsOccupied) {
+					homeIsOccupied = false;
+					updateIftttWithHomeStatus(homeIsOccupied);
+				}
+			} else {
+				if (!homeIsOccupied) {
+					homeIsOccupied = true;
+					updateIftttWithHomeStatus(homeIsOccupied);
+				}
+			}
+		}
+	}
 
-    function setIsHomeEnabled(enabled, hoursToPause) {
-        isHomeAwayEnabled = enabled;
-        if (hoursToPause) {
-            clearTimeout(homeEnabledTimeout);
-            homeEnabledTimeout = setTimeout(function() {
-                isHomeAwayEnabled = true;
-                updateIftttWithHomeStatus(isHomeAwayEnabled);
-            }, hoursToPause * 60 * 1000);
-        }
-    }
+	function setIsHomeEnabled(enabled, hoursToPause) {
+		isHomeAwayEnabled = enabled;
 
-    updateIftttWithHomeStatus(false);
+		if (hoursToPause) {
+			clearTimeout(homeEnabledTimeout);
+			homeEnabledTimeout = setTimeout(function() {
+				isHomeAwayEnabled = true;
+				updateIftttWithHomeStatus(isHomeAwayEnabled);
+			}, hoursToPause * 60 * 1000);
+		}
+	}
 
-    function updateIftttWithHomeStatus(isHome) {
-        var homeAwayText = isHome ? 'home' : 'away';
-        var url = messengerInfo.iftttHomeAway.baseUrl + `${homeAwayText}/with/key/`;
-        url += messengerInfo.iftttHomeAway.ApiKey;
-        var options = {
-            method: 'POST',
-            uri: url,
-            json: true
-        };
+	//updateIftttWithHomeStatus(false);
 
-        rp(options)
-            .then(function(parsedBody) {
-                // POST succeeded...
-                logger.debug(`Update ifttt with status ${homeAwayText}`);
-            })
-            .catch(function(err) {
-                // POST failed...
-                logger.error(`Failed: Update ifttt with status ${homeAwayText}`);
-            });
-    }
+	function updateIftttWithHomeStatus(isHome) {
+		var homeAwayText = isHome ? 'home' : 'away';
+		var url = messengerInfo.iftttHomeAway.baseUrl + `${homeAwayText}/with/key/`;
+		url += messengerInfo.iftttHomeAway.ApiKey;
+		var options = {
+			method: 'POST',
+			uri: url,
+			json: true
+		};
 
-    return {
-        garageIsOpen: garageIsOpen,
-        toggleGarageDoor: toggleGarageDoor,
-        toggleGarageOpenAlert: toggleGarageOpenAlert,
-        toggleGarageOpenAlertSecondPerson: toggleGarageOpenAlertSecondPerson,
-        setIsHomeEnabled: setIsHomeEnabled
-    };
+		rp(options)
+			.then(function(parsedBody) {
+				// POST succeeded...
+				logger.debug(`Update ifttt with status ${homeAwayText}`);
+			})
+			.catch(function(err) {
+				// POST failed...
+				logger.error(`Failed: Update ifttt with status ${homeAwayText}`);
+			});
+	}
+
+	return {
+		garageIsOpen: garageIsOpen,
+		toggleGarageDoor: toggleGarageDoor,
+		toggleGarageOpenAlert: toggleGarageOpenAlert,
+		toggleGarageOpenAlertSecondPerson: toggleGarageOpenAlertSecondPerson,
+		setIsHomeEnabled: setIsHomeEnabled
+	};
 };
