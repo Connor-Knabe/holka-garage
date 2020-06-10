9ab2913a229b012bd822b754645746b61441d001
Connor Knabe <ConnorKnabe@gmail.com>
2020-06-10 06:27:33 -0500

    larger text

--- controllers/routes.js
+++ controllers/routes.js
@@ -68,7 +68,7 @@ module.exports = function(app, logger, io, debugMode) {
 			}
 		}
 
-		io.sockets.emit('garageTimer', `${options.minsToWaitAfterLeavingHouseForGPSOpen} mins`);
+		io.sockets.emit('garageTimer', `${options.minsToWaitAfterLeavingHouseForGPSOpen} mins away`);
 
 		if (app.get('takingVideo')) {
 			io.sockets.emit('garageOpenStatus', 'Recording video');
