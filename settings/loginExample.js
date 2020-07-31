28d7f6a5ac362d9bd532d1199f9ea8d72e69a8f3
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2020-07-31 11:39:37 -0500

    updating loginExample

--- settings/loginExample.js
+++ settings/loginExample.js
@@ -13,16 +13,7 @@ module.exports.users = [
 	}
 ];
 
-/*
-module.exports.username = 'username for website';
-module.exports.password = 'password for website';
-
-module.exports.username2 = 'username2 for website';
-module.exports.password2 = 'password2 for website';
-*/
 module.exports.secret = '23094u2noiadsjfsdf';
-//module.exports.secretCookie = 'assdasdfj0i20i3j2ref0wejfi0fh97adhf!';
-//module.exports.secretAdminCookie = 'aksldjfoaisjdf0asjdf0';
 module.exports.sslPath = '/etc/letsencrypt/live/domain.com/';
 module.exports.acmeChallenge = 'al;sjdfas0idfjoasjdflksjdfokjasi0dfj0asdjf';
 module.exports.acmeChallengeKey = 'alksjdf0iasjdf0ijas0dfj0iasjdfaskldjflkajsdfja.asjdlfkjaklsdjflkjaslkdjf';
