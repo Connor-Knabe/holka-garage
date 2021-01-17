4a85c52053923e790a87b6846dec21710a7c984b
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2021-01-17 06:10:47 -0600

    cleaning up formatting

--- services/iot.js
+++ services/iot.js
@@ -176,7 +176,7 @@ module.exports = function(app, debugMode, io, logger, video, messenger, hue, cro
 		var months = (yearsTillNewSpring % 1)*12;
 		var days = (months % 1)*31;
 
-		return `${Math.floor(years)} years ${Math.floor(months)} months, days${Math.floor(days)} spring life left`;
+		return `${Math.floor(years)} years, ${Math.floor(months)} months, days ${Math.floor(days)}`;
 	}
 
 	function garageAlertStillOpenCheck(timeUntilAlert, timeOut, shouldCall) {
