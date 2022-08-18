c8677739ae9f0f3803bb14600e8880681d25349e
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2022-08-18 14:29:26 -0500

    using awayurl

--- services/iot.js
+++ services/iot.js
@@ -404,7 +404,7 @@ module.exports = function(app, debugMode, io, logger, video, messenger, hue, cro
 		function() {
 			if (homeAway.Status.isAway()) {
 				//turn off specific iot devices that may be on a schedule to turn on at top of the hour
-				messenger.sendIftt(null, 'set away', messengerInfo.iftttGarageSetAway2Url);
+				messenger.sendIftt(null, 'set away', messengerInfo.iftttGarageSetAwayUrl);
 			}
 		},
 		null,
