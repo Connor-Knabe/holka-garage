25eb2c3309d99b8096f9bbd6d25855e72d3c2355
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2022-11-02 17:39:37 -0500

    updating status

--- controllers/websiteAuthRoutes.js
+++ controllers/websiteAuthRoutes.js
@@ -22,9 +22,9 @@ module.exports = function(app, logger, io, hue, messenger, video, authService, h
 	app.use(authService.authChecker);
 
 	app.get('/status', function(req, res) {
-		var garageOpenClosed = iot.garageIsOpen() ? "O🔓": "C🔒";
-		var personOneAway = homeAway.isPersonAway(false) ? "A🚗": "H🏡";
-		var personTwoAway = homeAway.isPersonAway(true) ? "A🚗": "H🏡";
+		var garageOpenClosed = iot.garageIsOpen() ? "O": "C";
+		var personOneAway = homeAway.isPersonAway(false) ? "A": "H";
+		var personTwoAway = homeAway.isPersonAway(true) ? "A": "H";
 		res.send(`${garageOpenClosed}|1:${personOneAway}|2: ${personTwoAway}`);
 	});
 
