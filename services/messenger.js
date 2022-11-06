7efdc5a4038521db26d3daa082c091f2de5f26ea
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2022-11-06 01:12:30 -0600

    send message on boot fix

--- services/messenger.js
+++ services/messenger.js
@@ -77,6 +77,7 @@ module.exports = function(logger, debugMode) {
 	}
 
 	function send(shouldSend = true, numbers, msgContent, sendPicture = false, btnPress = false) {
+		logger.debug('send', shouldSend);
 		if (!shouldSend) {
 			return;
 		}
