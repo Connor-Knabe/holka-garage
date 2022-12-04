3a332352d6b3b08d203ac0b6dda42a9c819f33d3
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2022-12-04 16:44:31 -0600

    whitespace

--- public/socket.js
+++ public/socket.js
@@ -113,7 +113,7 @@ socket.on('personTwoAway', function (status) {
 socket.on('personOneName', function (status) {
     if (status) {
         if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
-            $("#personOneName").text("."+status);
+            $("#personOneName").text(" "+status);
         } else {
             $("#personOneName").text(status);
         }
