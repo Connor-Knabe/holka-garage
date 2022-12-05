68edaf78208ab9f8730367381500a82fe3bdd443
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2022-12-05 17:05:08 -0600

    remove safari check

--- public/socket.js
+++ public/socket.js
@@ -109,11 +109,12 @@ socket.on('personTwoAway', function (status) {
 
 socket.on('personOneName', function (status) {
     if (status) {
-        if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
-            $("#personOneName").text("."+status);
-        } else {
-            $("#personOneName").text(status);
-        }
+        // if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
+        //     $("#personOneName").text("."+status);
+        // } else {
+        //     $("#personOneName").text(status);
+        // }
+        $("#personOneName").text(status);
 
     
         $("#personOneName").show();
