b7acf9eaaeaf54501dfdcd66fefde68990ddc783
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2023-01-26 08:15:04 -0600

    logging req.body for debugging

--- controllers/gpsAuthRoutes.js
+++ controllers/gpsAuthRoutes.js
@@ -110,6 +110,7 @@ module.exports = function(app, logger, messenger, homeAway, bodyParser, iot) {
 		const isPersonTwo = false;
 		var gpsKey = isPersonTwo ? login.gpsPersonTwoKey : login.gpsPersonOneKey;
 		var personText = isPersonTwo ? 'personTwo' : 'personOne';
+		logger.debug('req.body', req.body);
 		if (req.body && typeof req.body == "string" && req.body.includes(gpsKey)) {
 			homeAway.setPersonAway(isPersonTwo);
 			iot.activateGarageGpsOpenAwayTimer(isPersonTwo);
